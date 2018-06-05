8b533b42d565deb7d854ec52a9995ec4195f988b
Ivan Eduardo Felix Herrera <ivanfex@Ivans-MacBook-Pro.local>
2018-06-05 16:17:00 -0400

    Added error handling and cleaned logs in the product routes

--- server/api/products.js
+++ server/api/products.js
@@ -1,13 +1,12 @@
 const router = require('express').Router()
 const { Product } = require('../db/models')
 
-console.log(Product);
-
 router.get('/', (req, res, next) => {
     Product.findAll({})
     .then(allProducts => {
         res.send(allProducts)
     })
+    .catch(next)
 })
 
 module.exports = router
