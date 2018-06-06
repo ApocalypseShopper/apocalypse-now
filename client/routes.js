07b00db4c1270da74aafa3461e1106bf41807e34
Jessie De La Cruz Santos <jessdelacruzsantos@gmail.com>
2018-06-06 18:39:44 -0400

    We introduced a reducer for cart, with thunks and actions. We also created a cart component with hard coded data

--- client/routes.js
+++ client/routes.js
@@ -7,6 +7,7 @@ import {me} from './store'
 import AllProducts from './components/AllProducts'
 import SingleProduct from './components/SingleProduct'
 import AddProducts from './components/AddProduct'
+import Cart from './components/Cart'
 
 /**
  * COMPONENT
@@ -27,6 +28,7 @@ class Routes extends Component {
         <Route exact path='/products' component={AllProducts} />
         <Route exact path='/products/add' component={AddProducts} />
         <Route path='/products/:productId' component={SingleProduct} />
+        <Route path='/cart' component={Cart} />
         {
           isLoggedIn &&
             <Switch>
