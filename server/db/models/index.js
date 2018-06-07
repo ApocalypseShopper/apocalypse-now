8f5b4e568fc968c2314c3495a621230bc959ea1a
Thanh-Lai <tlai1122@gmail.com>
2018-06-07 19:22:39 -0400

    Created(index model) Created Product Category association

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,6 +2,7 @@ const User = require('./user')
 const Product = require('./product')
 const OrderItem = require('./orderItem')
 const Order = require('./orders')
+const Category = require('./category')
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
@@ -16,15 +17,13 @@ const Order = require('./orders')
  * instead of: const User = require('../db/models/user')
  */
 
-OrderItem.belongsTo(Product)
-
-OrderItem.belongsTo(Order)
-Order.hasMany(OrderItem)
-
+Category.belongsToMany(Product, {through: 'Product-Category'})
+Product.belongsToMany(Category, {through: 'Product-Category'})
 
 module.exports = {
   User,
   Product,
   OrderItem,
-  Order
+  Order,
+  Category
 }
