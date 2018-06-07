f57ff269a908b3cf400ab0ce56ac233f7bc9b027
Ivan Eduardo Felix Herrera <ivanfex@Ivans-MacBook-Pro.local>
2018-06-07 13:16:15 -0400

    Reformatted

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,99 +1,86 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { fetchProducts } from '../store/product';
+import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import { fetchProducts } from '../store/product'
 
 /**
  * COMPONENT
  */
 class AllProducts extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor(props){
+    super(props)
     this.state = {
-      catInput: '',
+      catInput : '',
       nameInput: '',
-      catList: [],
-      nameList: [],
-    };
-    this.handleChange = this.handleChange.bind(this);
+      catList : [],
+      nameList : []
+    }
+    this.handleChange = this.handleChange.bind(this)
   }
 
-  handleChange(event) {
-    const name = event.target.name;
-    const value = event.target.value;
-    const filter = name === 'catInput' ? value.split(' ') : this.state.catList;
-    const filterName =
-      name === 'nameInput' ? value.split(' ') : this.state.nameList;
+  handleChange (event) {
+    const name = event.target.name
+    const value = event.target.value
+    const filter = name === 'catInput' ? value.split(' ') : this.state.catList
+    const filterName = name === 'nameInput' ? value.split(' ') : this.state.nameList
 
     this.setState({
-      [name]: value,
-      catList: filter,
-      nameList: filterName,
-    });
+      [name] : value,
+      catList : filter,
+      nameList: filterName
+    })
   }
 
-  componentDidMount() {
-    this.props.fetchProductsList();
+  componentDidMount(){
+    this.props.fetchProductsList()
   }
 
-  render() {
-    const selectedCategory = this.state.catList;
-    const selectedName = this.state.nameList;
+  render(){
+    const selectedCategory = this.state.catList
+    const selectedName = this.state.nameList
 
-    const products = this.props.products || [];
+    const products = this.props.products || []
 
-    const categoryFilteredProducts = products.filter(product =>
-      product.category.some(cat => selectedCategory.indexOf(cat) > -1)
-    );
-    const firstFilter = categoryFilteredProducts.length
-      ? categoryFilteredProducts
-      : products;
-
-    const nameFilteredProducts = firstFilter.length
-      ? firstFilter.filter(product =>
-          product.title.split(' ').some(prod => selectedName.indexOf(prod) > -1)
-        )
-      : [];
-    const displayProducts = nameFilteredProducts.length
-      ? nameFilteredProducts
-      : products;
+    const categoryFilteredProducts = products.filter(product => product.category.some(cat => selectedCategory.indexOf(cat) > -1) )
+    const firstFilter = categoryFilteredProducts.length ? categoryFilteredProducts : products 
+    
+    const nameFilteredProducts = firstFilter.length ? firstFilter.filter(product => product.title.split(' ').some(prod => selectedName.indexOf(prod) > -1) ) : []
+    const displayProducts = nameFilteredProducts.length ? nameFilteredProducts : products
     return (
       <div>
-        <input name="catInput" onChange={this.handleChange} />
-        <input name="nameInput" onChange={this.handleChange} />
+        <input name='catInput' onChange={this.handleChange}/>
+        <input name='nameInput' onChange={this.handleChange}/>
         <ul>
-          {displayProducts.map(product => {
-            return (
-              <li key={product.id}>
-                <h3>
-                  categories: {product.category.join(' ')} name: {product.title}{' '}
-                  costs {`$${product.price}`} and we have {product.quantity} on
-                  stock
-                </h3>
-              </li>
-            );
-          })}
+          {
+            displayProducts.map(product => {
+              return (
+                <li key={product.id}>
+                  <h3>categories: {product.category.join(' ')}  name: {product.title} costs {`$${product.price}`} and we have {product.quantity} on stock</h3>
+                </li>
+              )
+            })
+          }
         </ul>
       </div>
-    );
+    )
   }
 }
 
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
-    products: state.products.allProducts,
-  };
-};
+    products: state.products.allProducts
+  }
+}
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     fetchProductsList: () => {
-      dispatch(fetchProducts());
-    },
-  };
-};
+      dispatch(fetchProducts())
+    }
+  }
+}
 
-export default connect(mapState, mapDispatch)(AllProducts);
+export default connect(mapState, mapDispatch)(AllProducts)
