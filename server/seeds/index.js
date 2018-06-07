bf76f1840473720b4e623690ce88b2a1860ac38d
Ivan Eduardo Felix Herrera <ivanfex@Ivans-MacBook-Pro.local>
2018-06-07 11:19:51 -0400

    Created seed file and added categories setter

---
+++ server/seeds/index.js
@@ -0,0 +1,10 @@
+const {Product} = require('../db/models')
+const seed = require('./productsSeed')
+
+module.exports = async () => {
+    let products = seed.map(product => {
+        Product.create(product)
+    })
+
+    await Promise.all(products)
+}
