608c93b210b6c9afbe3c9231c98a34128f6cabc9
Ivan Eduardo Felix Herrera <ivanfex@Ivans-MacBook-Pro.local>
2018-06-11 12:21:38 -0400

    Refactor of seed file for products and categories

--- server/index.js
+++ server/index.js
@@ -94,7 +94,7 @@ const startListening = () => {
   require('./socket')(io)
 }
 
-const syncDb = () => db.sync()
+const syncDb = () => db.sync({force: true})
 
 // This evaluates as true when this file is run directly from the command line,
 // i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
@@ -103,7 +103,7 @@ const syncDb = () => db.sync()
 if (require.main === module) {
   sessionStore.sync()
     .then(syncDb)
-    // .then(seed)
+    .then(seed)
     .then(createApp)
     .then(startListening)
 } else {
--- server/seeds/index.js
+++ server/seeds/index.js
@@ -1,11 +1,37 @@
-const {Product} = require('../db/models')
+const {Product,Category} = require('../db/models')
 const seed = require('./productsSeed')
 
 module.exports = async () => {
-    let products = seed.map(product => {
-        Product.create(product)
+    let products = seed.map(async product => {
+        const productCreated = await Product.create({
+            title: product.title,
+            description: product.description,
+            price: product.price,
+            quantity: product.quantity
+        })
+
+        
+        const categoriesList = product.categories.split(' ')
+        const cats = await Promise.all(categoriesList.map(cat => {
+            return Category.findOrCreate({
+                where: {
+                    name: cat
+                },
+                defaults: { name: cat }
+            })
+        }))
+        
+        let mappedCategories = cats.map(cat => {
+            return cat[0].id
+        })
+        
+        mappedCategories = mappedCategories.filter((catId,idx) => {
+            return mappedCategories.indexOf(catId) !== idx
+        })
+        
+        await productCreated.addCategories(mappedCategories)  
     })
 
     await Promise.all(products)
-    console.log("!!!!!!!!!!!!SEEDED!!!!!!!!!!")
+    
 }
