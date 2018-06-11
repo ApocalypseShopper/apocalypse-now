26fbcd9025d02822cb4fd837854f14a0b97db4a4
Ivan Eduardo Felix Herrera <ivanfex@Ivans-MacBook-Pro.local>
2018-06-11 19:34:37 -0400

    Refactored navbar to add antd components

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,54 +1,75 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
+import {withRouter} from 'react-router-dom'
 import {logout} from '../store'
 
 import { Menu, Icon } from 'antd';
-const SubMenu = Menu.SubMenu;
-const MenuItemGroup = Menu.ItemGroup;
 
 class Navbar extends React.Component{
   constructor(){
     super()
     this.state = {
-      current: 'mail'
+      current: 'products'
     }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  componentDidMount(){
+    this.setState({
+      current: this.props.location.pathname.slice(1)
+    })
+  }
+
+  handleClick = (e) => {
+    console.log('click ', e);
+    this.setState({
+      current: e.key,
+    });
+    this.props.history.push(`/${e.key}`)
   }
 
   render(){
-    const {handleClick, isLoggedIn} = this.props
+    const {isLoggedIn} = this.props
+    console.log(this.props)
     return (
       <div>
-        {/* <h1>BOILERMAKER</h1>
         <nav>
           {isLoggedIn ? (
-            <div>
-              <Link to="/home">Home</Link>
-              <a href="#" onClick={handleClick}>
-                Logout
-              </a>
-            </div>
+            <Menu
+              onClick={this.handleClick}
+              selectedKeys={[this.state.current]}
+              mode="horizontal"
+              theme="dark"
+            >
+              <Menu.Item key="products">
+                <Icon type="tool" />Products
+              </Menu.Item>
+              <Menu.Item key="logout">
+                <Icon type="logout" />Log Out
+              </Menu.Item>
+            </Menu>
           ) : (
-            <div>
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Sign Up</Link>
-            </div>
+            <Menu
+              onClick={this.handleClick}
+              selectedKeys={[this.state.current]}
+              mode="horizontal"
+              theme="dark"
+            >
+              <Menu.Item key="products">
+                <Icon type="tool" />Products
+              </Menu.Item>
+              <Menu.Item key="login">
+                <Icon type="login" />Log In
+              </Menu.Item>
+              <Menu.Item key="signup">
+                <Icon type="user-add" />Sign Up
+              </Menu.Item>
+            </Menu>
           )}
         </nav>
-        <hr /> */}
-        <Menu
-        onClick={this.handleClick}
-        selectedKeys={[this.state.current]}
-        mode="horizontal"
-      >
-          <Menu.Item key="mail">
-            <Icon type="mail" />Home
-          </Menu.Item>
-          <Menu.Item key="app" disabled>
-            <Icon type="appstore" />Log Out
-          </Menu.Item>
-        </Menu>
+      
+
       </div>
     )
   }
@@ -71,7 +92,7 @@ const mapDispatch = dispatch => {
   }
 }
 
-export default connect(mapState, mapDispatch)(Navbar)
+export default withRouter(connect(mapState, mapDispatch)(Navbar))
 
 /**
  * PROP TYPES
